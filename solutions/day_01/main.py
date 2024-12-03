087ce9afc80d7b0764a7dd5a34bab986017b7f15
Jared A Ivory <26067982+jaredaivory@users.noreply.github.com>
2024-12-03 02:53:46 -0500

    day01 part2

--- solutions/day_01/main.py
+++ solutions/day_01/main.py
@@ -1,10 +1,18 @@
 
+from collections import Counter
 from typing import List, Tuple
 import heapq
 
 
-class PartOneSolution:
+def import_input(path: str) -> Tuple[List[int], List[int]]:
+    with open(path, 'r') as file:
+        arr_0, arr_1 = [list(arr) for arr in zip(
+            *[[int(num) for num in line.split()] for line in file.readlines()])]
+        return (arr_0, arr_1)
+    pass
 
+
+class PartOneSolution:
     def solve(arr1: List[int], arr2: List[int]) -> int:
         heapq.heapify(arr1)
         heapq.heapify(arr2)
@@ -14,14 +22,23 @@ class PartOneSolution:
             total_distance += abs(heapq.heappop(arr1) - heapq.heappop(arr2))
         return total_distance
 
-    def import_input(path: str) -> Tuple[List[int], List[int]]:
-        with open(path, 'r') as file:
-            arr_0, arr_1 = [list(arr) for arr in zip(
-                *[[int(num) for num in line.split()] for line in file.readlines()])]
-            return (arr_0, arr_1)
-        pass
+
+class PartTwoSolution:
+    def solve(arr1: List[int], arr2: List[int]) -> int:
+        location_id_count = Counter(arr2)
+
+        similarity_score = 0
+
+        for location_id in arr1:
+            similarity_score += location_id_count[location_id] * \
+                location_id if location_id in location_id_count else 0
+
+        return similarity_score
 
 
 if __name__ == "__main__":
-    arr1, arr2 = PartOneSolution.import_input('puzzle_input.txt')
-    print(PartOneSolution.solve(arr1, arr2))
+    arr1, arr2 = import_input('puzzle_input.txt')
+    print(
+        f"Day 01 | Part One - Solution: {PartOneSolution.solve(arr1[::], arr2[::])}")
+    print(
+        f"Day 02 | Part Two - Solution: {PartTwoSolution.solve(arr1[::], arr2[::])}")
